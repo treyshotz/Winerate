78d1fe7043422448a5bcd83448e6981cf57ea7d8
Mads Lundegaard <madslun@madss-macbook-pro.home>
2020-07-01 23:15:45 +0200

    Started on wine class

--- src/main/java/models/Rating.java
+++ src/main/java/models/Rating.java
@@ -1,5 +1,8 @@
 package models;
 
+
+import java.util.Date;
+
 public class Rating {
 	
 	/*
@@ -8,4 +11,50 @@ public class Rating {
 	- Description
 	- Date
 	 */
+	private String author;
+	private int rating;
+	private String description;
+	private Date date;
+	
+	public Rating() {
+	}
+	
+	public Rating(String author, int rating, String description, Date date) {
+		this.author = author;
+		this.rating = rating;
+		this.description = description;
+		this.date = date;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
---
+++ src/main/java/models/Wine.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Wine extends Drink {
+	//TODO: Decide what to include in a wine object
+	
+	public Wine() {
+	}
+}
