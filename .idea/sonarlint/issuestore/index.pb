c755d6f114b8c7af0633d92f564439d0a230753a
Mads Lundegaard <madslun@madss-macbook-pro.home>
2020-07-06 20:23:41 +0200

    Added fields to wine

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+9
+	readme.md,2/7/275d783e298228506068436512433d343feb52aa
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/models/Rating.java,0/3/03ded298c75663f793c34ffbcf7602a237a11947
+S
+#src/main/java/models/Drinkable.java,a/6/a643856e79a2cc0705a84767f2958baabe18138d
+O
+src/main/java/models/Drink.java,f/2/f2fd0c3986b420fc07d1f37aafb4fcd410b50760
+N
+src/main/java/models/Wine.java,d/a/daf6a23dd67846843c5c8826ca045a5cd91ae6b7
--- src/main/java/models/Wine.java
+++ src/main/java/models/Wine.java
@@ -1,8 +1,76 @@
 package models;
 
+import java.util.List;
+
 public class Wine extends Drink {
 	//TODO: Decide what to include in a wine object
+	//Redwine, whitewine, rose, port, sparkling
+	private String type;
+	private int age;
+	private String country;
+	private String region;
+	private boolean organic;
+	//A wine can have several different grapes
+	private List<String> grapes;
 	
 	public Wine() {
+	}
+	
+	public Wine(int productId, String name, double alcohol, double volume, double price, String description, double avgRating, List<Rating> ratings, String type, int age, String country, String region, boolean organic, List<String> grapes) {
+		super(productId, name, alcohol, volume, price, description, avgRating, ratings);
+		this.type = type;
+		this.age = age;
+		this.country = country;
+		this.region = region;
+		this.organic = organic;
+		this.grapes = grapes;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getRegion() {
+		return region;
+	}
+	
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	
+	public boolean isOrganic() {
+		return organic;
+	}
+	
+	public void setOrganic(boolean organic) {
+		this.organic = organic;
+	}
+	
+	public List<String> getGrapes() {
+		return grapes;
+	}
+	
+	public void setGrapes(List<String> grapes) {
+		this.grapes = grapes;
 	}
 }
