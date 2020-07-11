bf116803221e20e5fd7dce6f8de933238eab657f
Stian Mogen <stianfmo@stud.ntnu.no>
2020-07-11 11:55:20 +0200

    added search methods
    search search facking search

--- src/main/java/services/DrinkService.java
+++ src/main/java/services/DrinkService.java
@@ -2,9 +2,12 @@ package services;
 
 import models.Drink;
 import models.Rating;
+import models.Wine;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DrinkService {
 
@@ -33,4 +36,27 @@ public class DrinkService {
     public void registerRating(Drink drink, Rating rating){
         drinksList.stream().filter(p -> p.equals(drink)).findAny().ifPresent(drink1 -> drink1.addRating(rating));
     }
+
+    /**
+     * Searches through all drinks to find drinks with names containing the parameter
+     * @param name
+     * @return list with all drinks containing 'name'
+     */
+    public List<Drink> searchName(String name){
+        return drinksList.stream().filter(p -> p.getName().toLowerCase().contains(name.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public List<Drink> searchType(String type){
+        //Todo method searching for all drinks in a specific subclass "type"
+        return null;
+    }
+
+    public List<Wine> searchWineType(String type){
+        List<Drink> winelist = new ArrayList<>();
+        winelist = drinksList.stream().filter(p -> p instanceof Wine).collect(Collectors.toList());
+        //need to cast this properly, but cant be bothered right now
+        //return winelist.stream().filter(p -> p.getType.toLowerCase()).equals(type.toLowerCase())
+        return null;
+    }
+
 }
